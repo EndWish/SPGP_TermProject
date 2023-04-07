dd808a66a203af769c667e5bdd5b09e3359d059b
Si In Kim <ksi8548@gmail.com>
2023-04-07 21:59:00 +0900

    안드로이드 빈 프로젝트 추가.
    안드로이드 빈 프로젝트 추가.

---
+++ EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/MainActivity.java
@@ -0,0 +1,14 @@
+package tukorea2018180009.ac.kr.example.equipmentcollector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
