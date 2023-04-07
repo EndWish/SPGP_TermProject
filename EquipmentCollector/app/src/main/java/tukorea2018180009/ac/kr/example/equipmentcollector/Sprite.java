e6dae7e1e4e522eb45c4dd6f91b8c74132093760
Si In Kim <ksi8548@gmail.com>
2023-04-08 02:36:30 +0900

    sprite에 pivot을 추가시키고, 타이틀화면에 마을이미지를 출력함.
    sprite에 pivot을 추가시키고, 타이틀화면에 마을이미지를 출력함.

--- EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/Sprite.java
+++ EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/Sprite.java
@@ -7,10 +7,12 @@ import android.graphics.RectF;
 public class Sprite implements IGameObject {
     protected Bitmap bitmap;
     protected RectF dstRect = new RectF();
-    protected float x, y, width, height;
-    public Sprite(int bitmapResId, float cx, float cy, float width, float height) {
+    protected float x, y, width, height, pivotx, pivoty;
+    public Sprite(int bitmapResId, float cx, float cy, float width, float height, float pivotx, float pivoty) {
         this.x = cx;
         this.y = cy;
+        this.pivotx = pivotx;
+        this.pivoty = pivoty;
         this.width = width;
         this.height = height;
         if (bitmapResId != 0) {
@@ -26,7 +28,7 @@ public class Sprite implements IGameObject {
     protected void fixDstRect() {
         float half_width = width / 2;
         float half_height = height / 2;
-        dstRect.set(x - half_width, y - half_height, x + half_width, y + half_height);
+        dstRect.set(x - width * pivotx, y - height * pivoty, x + width - width * pivotx, y + height - height * pivoty);
     }
 
     @Override
--- EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/TitleScene.java
+++ EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/TitleScene.java
@@ -5,16 +5,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 
 public class TitleScene extends BaseScene{
-    private Paint paint;    // 씬이 제대로 만들어져서 push되는지 확인하기 위해 임시로 추가한 페인트
 
     public TitleScene() {
-        paint = new Paint();
-        paint.setColor(Color.BLUE);
+        Sprite backgroundObj = new Sprite(R.mipmap.jpg_village_background, 0,0,16,9,0,0);
+        add(backgroundObj);
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawRect(1,1, 15, 8, paint);
         super.draw(canvas);
     }
 }
