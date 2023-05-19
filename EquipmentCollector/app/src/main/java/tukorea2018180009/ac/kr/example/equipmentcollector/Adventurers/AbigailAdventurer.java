3b90dd73c0f73f378367f9234cc3010977ef4c6f
Si In Kim <ksi8548@gmail.com>
2023-05-19 23:02:40 +0900

    AbigailAdventurer추가 및 Arthur능력치 조정.
    AbigailAdventurer추가 및 Arthur능력치 조정.

--- EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/Adventurers/AbigailAdventurer.java
+++ EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/Adventurers/AbigailAdventurer.java
@@ -5,17 +5,16 @@ import android.graphics.Bitmap;
 import java.util.ArrayList;
 
 import tukorea2018180009.ac.kr.example.equipmentcollector.Equipment.Equipment;
-import tukorea2018180009.ac.kr.example.equipmentcollector.Memory.BitmapPool;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Equipment.PlainSwordEquipment;
-import tukorea2018180009.ac.kr.example.equipmentcollector.Object;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Memory.BitmapPool;
 import tukorea2018180009.ac.kr.example.equipmentcollector.R;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Skills.RushSkill;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Skills.SlashSkill;
 
-public class ArthurAdventurer extends Adventurer{
-    private static final String name = "Arthur";
-    private static final Bitmap icon = BitmapPool.get(R.mipmap.png_icon_adventurer_arthur_256x256);
-    private static final Bitmap profile = BitmapPool.get(R.mipmap.png_profile_adventurer_arthur_256x384);
+public class AbigailAdventurer extends Adventurer{
+    private static final String name = "Abigail";
+    private static final Bitmap icon = BitmapPool.get(R.mipmap.png_icon_adventurer_abigail_256x256);
+    private static final Bitmap profile = BitmapPool.get(R.mipmap.png_profile_adventurer_abigail_256x384);
 
     @Override
     public String getName() {
@@ -32,27 +31,27 @@ public class ArthurAdventurer extends Adventurer{
 
     @Override
     protected void initBasicStatus() {
-        basicStatus.set(Status.Type.hpm, 100);
+        basicStatus.set(Status.Type.hpm, 70);
 
         basicStatus.set(Status.Type.essentialSkillSpeed, 2);
         basicStatus.set(Status.Type.normalSkillSpeed, 2);
         basicStatus.set(Status.Type.ultimateSkillSpeed, 2);
 
-        basicStatus.set(Status.Type.slashPower, 10);
-        basicStatus.set(Status.Type.piercePower, 5);
-        basicStatus.set(Status.Type.impactPower, 3);
+        basicStatus.set(Status.Type.slashPower, 0);
+        basicStatus.set(Status.Type.piercePower, 0);
+        basicStatus.set(Status.Type.impactPower, 0);
 
-        basicStatus.set(Status.Type.magicPower, 0);
-        basicStatus.set(Status.Type.holyPower, 0);
+        basicStatus.set(Status.Type.magicPower, 4);
+        basicStatus.set(Status.Type.holyPower, 10);
         basicStatus.set(Status.Type.cursedPower, 0);
 
-        basicStatus.set(Status.Type.slashDefense, 10);
-        basicStatus.set(Status.Type.pierceDefense, 8);
-        basicStatus.set(Status.Type.impactDefense, 8);
+        basicStatus.set(Status.Type.slashDefense, 3);
+        basicStatus.set(Status.Type.pierceDefense, 3);
+        basicStatus.set(Status.Type.impactDefense, 3);
 
-        basicStatus.set(Status.Type.magicResistance, 5);
+        basicStatus.set(Status.Type.magicResistance, 10);
         basicStatus.set(Status.Type.holyResistance, 10);
-        basicStatus.set(Status.Type.cursedResistance, 3);
+        basicStatus.set(Status.Type.cursedResistance, 7);
 
 
     }
@@ -66,7 +65,6 @@ public class ArthurAdventurer extends Adventurer{
     @Override
     public ArrayList<Equipment> getRewardEquipments() {
         ArrayList<Equipment> rewards = new ArrayList<Equipment>();
-        rewards.add(new PlainSwordEquipment());
         return rewards;
     }
     @Override
