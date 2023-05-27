40c6c55c2d3436bbbfc154e0d8813a46bba39b09
Si In Kim <ksi8548@gmail.com>
2023-05-27 20:17:25 +0900

    babyHydra 몬스터 추가.
    babyHydra 몬스터 추가.

--- EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/ExpenditionAreaInfo/Forest1_ExpeditionAreaInfo.java
+++ EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/ExpenditionAreaInfo/Forest1_ExpeditionAreaInfo.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 import tukorea2018180009.ac.kr.example.equipmentcollector.Adventurers.ArthurAdventurer;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Memory.BitmapPool;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Monster.BabyHydraMonster;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Monster.GrizzlyeerMonster;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Monster.IvernMonster;
 import tukorea2018180009.ac.kr.example.equipmentcollector.Monster.LiliumSerpensMonster;
@@ -36,37 +37,42 @@ public class Forest1_ExpeditionAreaInfo extends ExpeditionAreaInfo {
         ArrayList<BattleProfile> result = new ArrayList<>();
 
         final float percentage = (float)(Math.random() * 100.0);
-        if(percentage <= 20){
+        if(percentage <= 10){
             // 파티 생성 (멀록, 멀록, 멀록)
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));   // 1열
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));   // 2열
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));   // 3열
         }
-        else if(percentage <= 100){
-            // 파티 생성 (릴리움, 릴리움)
-            result.add(new BattleProfile(new GrizzlyeerMonster(), 0,0,150));
-        }
-        else if(percentage <= 40){
+        else if(percentage <= 20){
             // 파티 생성 (멀록, 멀록,)
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));
         }
-        else if(percentage <= 60){
+        else if(percentage <= 30){
             // 파티 생성 (멀록, 멀록, 이번)
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));
             result.add(new BattleProfile(new IvernMonster(), 0,0,150));
         }
-        else if(percentage <= 80){
+        else if(percentage <= 40){
             // 파티 생성 (멀록, 이번, 릴리움)
             result.add(new BattleProfile(new MurlocMonster(), 0,0,150));
             result.add(new BattleProfile(new LiliumSerpensMonster(), 0,0,150));
             result.add(new BattleProfile(new IvernMonster(), 0,0,150));
         }
-        else if(percentage <= 100){
+        else if(percentage <= 50){
             // 파티 생성 (릴리움, 릴리움)
             result.add(new BattleProfile(new LiliumSerpensMonster(), 0,0,150));
             result.add(new BattleProfile(new LiliumSerpensMonster(), 0,0,150));
+        }
+        else if(percentage <= 60){
+            // 파티 생성
+            result.add(new BattleProfile(new BabyHydraMonster(), 0,0,150));
+            result.add(new BattleProfile(new LiliumSerpensMonster(), 0,0,150));
+        }
+        else if(percentage <= 100){
+            // 파티 생성
+            result.add(new BattleProfile(new GrizzlyeerMonster(), 0,0,150));
         }
 
         return result;
