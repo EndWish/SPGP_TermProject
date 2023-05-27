bc1768269ce0adf5430d6ca259d1bda5756d8c17
Si In Kim <ksi8548@gmail.com>
2023-05-27 19:55:25 +0900

    GrizzlyeerMonster추가.
    GrizzlyeerMonster추가.

---
+++ EquipmentCollector/app/src/main/java/tukorea2018180009/ac/kr/example/equipmentcollector/Monster/GrizzlyeerMonster.java
@@ -0,0 +1,78 @@
+package tukorea2018180009.ac.kr.example.equipmentcollector.Monster;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+import tukorea2018180009.ac.kr.example.equipmentcollector.Adventurers.Adventurer;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Adventurers.Status;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Equipment.Equipment;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Equipment.PlainSwordEquipment;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Memory.BitmapPool;
+import tukorea2018180009.ac.kr.example.equipmentcollector.R;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Skills.RushSkill;
+import tukorea2018180009.ac.kr.example.equipmentcollector.Skills.SlashSkill;
+
+public class GrizzlyeerMonster extends Adventurer {
+    private static final String name = "Grizzlyeer";
+    private static final Bitmap icon = BitmapPool.get(R.mipmap.png_icon_adventurer_arthur_256x256);
+    private static final Bitmap profile = BitmapPool.get(R.mipmap.png_profile_monster_grizzlyeer_256x384);
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public Bitmap getIcon() {
+        return icon;
+    }
+    @Override
+    public Bitmap getProfile() {
+        return profile;
+    }
+
+    @Override
+    protected void initBasicStatus() {
+        basicStatus.set(Status.Type.hpm, 100);
+
+        basicStatus.set(Status.Type.essentialSkillSpeed, 2);
+        basicStatus.set(Status.Type.normalSkillSpeed, 2);
+        basicStatus.set(Status.Type.ultimateSkillSpeed, 2);
+
+        basicStatus.set(Status.Type.slashPower, 15);
+        basicStatus.set(Status.Type.piercePower, 3);
+        basicStatus.set(Status.Type.impactPower, 18);
+
+        basicStatus.set(Status.Type.magicPower, 0);
+        basicStatus.set(Status.Type.holyPower, 0);
+        basicStatus.set(Status.Type.cursedPower, 0);
+
+        basicStatus.set(Status.Type.slashDefense, 16);
+        basicStatus.set(Status.Type.pierceDefense, 14);
+        basicStatus.set(Status.Type.impactDefense, 18);
+
+        basicStatus.set(Status.Type.magicResistance, 15);
+        basicStatus.set(Status.Type.holyResistance, 15);
+        basicStatus.set(Status.Type.cursedResistance, 10);
+
+
+    }
+
+    @Override
+    protected void initSkills() {
+        //skills.add(new SlashSkill());
+        skills.add(new SlashSkill());
+    }
+
+    @Override
+    public ArrayList<Equipment> getRewardEquipments() {
+        ArrayList<Equipment> rewards = new ArrayList<Equipment>();
+        rewards.add(new PlainSwordEquipment());
+        return rewards;
+    }
+    @Override
+    public int getRewardGold() {
+        return 50;
+    }
+
+}
